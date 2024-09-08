cca89ed33479f129e013189cdd6286d88d6cc4b2
havancanh2003 <havancanh2003@gmail.com>
2024-09-08 23:42:24 +0700

    phần cấu hình Add project files.

---
+++ WebApiToCleanArchitecture/Application/Services/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using WebApiToCleanArchitecture.Application.DTOs;
+using WebApiToCleanArchitecture.Domain.Entities;
+using WebApiToCleanArchitecture.Domain.Interfaces;
+
+namespace WebApiToCleanArchitecture.Application.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper; 
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductDto>>(products);
+        }
+
+        public async Task<ProductDto> GetProductByIdAsync(Guid id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return null;
+
+            return _mapper.Map<ProductDto>(product); 
+        }
+
+        public async Task AddProductAsync(ProductDto productDto)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(productDto);
+                await _productRepository.AddAsync(product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task UpdateProductAsync(ProductDto productDto)
+        {
+            var product = _mapper.Map<Product>(productDto);
+            await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task DeleteProductAsync(Guid id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
+    }
+
+}
---
+++ WebApiToCleanArchitecture/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApiToCleanArchitecture.Domain.Entities;
+
+namespace WebApiToCleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(Guid id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Guid id);
+    }
+}
